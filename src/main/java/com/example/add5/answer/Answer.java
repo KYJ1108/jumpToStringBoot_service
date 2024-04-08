3cbb995ee15ca9eedfaf67f33f4ac3c20592e3eb
unknown <kimyeji546408@gmail.com>
2024-04-08 14:52:41 +0900

    spring3_8

---
+++ src/main/java/com/example/add5/answer/Answer.java
@@ -0,0 +1,29 @@
+package com.example.add5.answer;
+
+import com.example.add5.question.Question;
+import com.example.add5.user.SiteUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+    @ManyToOne
+    private SiteUser author;
+}
---
+++ src/main/java/com/example/add5/answer/AnswerService.java
@@ -0,0 +1,24 @@
+package com.example.add5.answer;
+
+import com.example.add5.question.Question;
+import com.example.add5.user.SiteUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content, SiteUser author) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+}
